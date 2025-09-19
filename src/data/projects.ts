3caa1ebe1db13fb7643f3680b3c9bec916d8261a
Maydara <cherif.mohamed86@gmail.com>
2025-09-19 20:56:16 +0100

    Update Links

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -16,7 +16,7 @@ export const projects: Project[] = [
     description: 'Modern e-commerce solution with Wordpress and WooCommerce, Flatesome theme, and Elementor builder, Newsletter subscription, and SEO optimization',
     image: '/images/fourpins.jpg',
     link: 'https://www.fourpins.com.tn/',
-    github: 'https://github.com/mohamed-cherif/ecommerce-platform',
+    github: '',
     technologies: ['Wordpress', 'WooCommerce', 'Elementor', 'Flatsome', 'PHP', 'MySQL'],
     category: ['web', 'e-commerce']
   },
@@ -26,7 +26,7 @@ export const projects: Project[] = [
     description: 'Modern Tunisian news website built with Wordpress and Newspaper theme, Elementor builder, Newsletter subscription, and SEO optimization',
     image: '/images/tunisie-telegraph.jpg',
     link: 'https://tunisie-telegraph.com/',
-    github: 'https://github.com/mohamed-cherif/task-manager',
+    github: '',
     technologies: ['Wordpress', 'Newspaper', 'Elementor', 'PHP', 'MySQL', 'SEO', 'i18n'],
     category: [ 'web' ]
   },
@@ -35,8 +35,9 @@ export const projects: Project[] = [
     title: 'MakeItOne an ETL app',
     description: 'A fully fledge ETL application developed with React and Spring Boot, it includes a designer canvas page that supports nodes drag & drop, wiring nodes, data manipulation, save and export feature and so much more',
     image: '/images/MakeItOne.jpg',
-    link: 'https://demo-restaurant.com',
-    github: 'https://github.com/mohamed-cherif/restaurant-site',
+    // link: 'https://www.linkedin.com/company/istars/',
+    link: 'http://istars.net/',
+    github: '',
     technologies: ['React', 'react-flow', 'Material-UI', 'redux-toolkit', 'Spring Boot', 'Microsoft SQL Server'],
     category: ['web', 'SaaS']
   },
@@ -45,8 +46,8 @@ export const projects: Project[] = [
     title: 'Kaspr an all in one lead prospecting tool',
     description: 'Kaspr is a chrome extension that helps you find and verify B2B email addresses directly from LinkedIn profiles. Kaspr\'s Dashoboard is a web application that allows you to manage your leads, campaigns, and account settings. Kaspr\'s Backoffice is a web application that allows staff members to manage their users, subscriptions, and credits.',
     image: '/images/kaspr.jpg',
-    link: 'https://demo-realestate.com',
-    github: 'https://github.com/mohamed-cherif/real-estate',
+    link: 'https://www.kaspr.io/',
+    github: '',
     technologies: ['react', 'express.js', 'Mongodb', 'Various APIs integrations'],
     category: ['web', 'chrome-extension', 'SaaS']
   },
@@ -55,8 +56,8 @@ export const projects: Project[] = [
     title: 'Cognism\'s front-end Design System',
     description: 'Design System for all Cognism front-end projects, the library is composed of 40+ high-quality components, they were designed in Figma, built with React, showcased in Storybook and delivered through npm packages',
     image: '/images/cognism.png',
-    link: 'https://demo-healthcare.com',
-    github: 'https://github.com/mohamed-cherif/healthcare-dashboard',
+    link: 'https://www.cognism.com/',
+    github: '',
     technologies: ['React', 'CSS', 'Storybook', 'npm', 'Figma'],
     category: ['web', 'SaaS']
   },
@@ -65,8 +66,8 @@ export const projects: Project[] = [
     title: 'SatisFactory feedback management tool',
     description: 'SatisFactory is a solution specialized in Feedback Management, it helps businesses collect, analyze, and act on customer feedback to improve customer experience and operational performance',
     image: '/images/satisfactory.png',
-    link: 'https://demo-finance.com',
-    github: 'https://github.com/mohamed-cherif/finance-app',
+    link: 'https://www.satisfactory.fr/',
+    github: '',
     technologies: ['PHP', 'Zend', 'MySQL', 'Angular', 'Various APIs integrations'],
     category: ['web', 'SaaS']
   }
--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -18,7 +18,7 @@ export const testimonials: Testimonial[] = [
     content: 'I worked with Mohamed on a Design System initiative within a remote team and found him to be an excellent team player. He consistently delivered high-quality work on time and quickly adapted to new programming languages. His dedication and focus on efficiency had a significant positive impact on our project. Mohamed is a skilled and reliable front-end developer who would be a great asset to any team.',
     rating: 5,
     avatar: '/images/David_Phillip.jpeg',
-    date: '2024-01-15'
+    date: '2024-09-15'
   },
   {
     id: '2',
@@ -28,6 +28,6 @@ export const testimonials: Testimonial[] = [
     content: 'Travailler avec Mohamed a été une excellente expérience. Il a su comprendre nos besoins et livrer un site de news moderne, fluide et parfaitement adapté à notre audience. La mise en ligne s’est faite dans les délais, avec une attention particulière portée à la performance et à la simplicité d’utilisation. Nous sommes très satisfaits du résultat et recommandons vivement ses services !',
     rating: 5,
     avatar: '/images/Jamal.jpg',
-    date: '2024-02-03'
+    date: '2021-05-03'
   }
 ];
